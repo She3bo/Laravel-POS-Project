e49a49bff0fc418e22439c68a84dbb64fcec83d6
Ahmed Shaban <ahmedshaban33ah@gmail.com>
2020-04-25 05:10:58 +0200

    add dashboard with languages

---
+++ resources/lang/en/dashboard.php
@@ -0,0 +1,12 @@
+<?php
+return[
+    "title"         => "Dashboard",
+    "dashboard"     => "Dashboard",
+    "admin"         => "Ahmed Shaban",
+    "category"      => "Category",
+    "logout"        => "Logout",
+    "control"       => "Home",
+    "contact"        => "Contact",
+    "search"        => "search",
+];
+?>
---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.dashboard.app')
+
+@section('content')
+     <h1>Hello from Dashboard</h1>
+@endsection
