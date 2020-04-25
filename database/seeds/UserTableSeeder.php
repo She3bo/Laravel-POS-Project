3a74b7fe5fc538b2487b35f65f0b78a71315dae5
Ahmed Shaban <ahmedshaban33ah@gmail.com>
2020-04-25 17:44:37 +0200

    add laratrust role permission

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = \App\User::create([
+            "first_name" => "super",
+            "last_name"  => "admin",
+            "email"      => "super_admin@app.com",
+            "password"   => bcrypt('12345678'),
+        ]);
+        $user->attachRole('super_admin');
+    }
+}
